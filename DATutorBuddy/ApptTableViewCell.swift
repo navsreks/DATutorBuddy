74bd566f5f17f9fab0fe8534bc97897ccbb64cd9
FirstName <navya@biztekinnovations.com>
2016-12-06 14:42:47 -0800

    added 4 appt files and modified storyboard

---
+++ DATutorBuddy/ApptTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ApptTableViewCell.swift
+//  DATutorBuddy
+//
+//  Created by Navya Rekapally on 12/6/16.
+//  Copyright © 2016 DeAnza. All rights reserved.
+//
+
+import UIKit
+
+class ApptTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
